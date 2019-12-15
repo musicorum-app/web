37716e8277f57f49d14b834dd9adb754bbf7d403
metehus <methidezeeni@gmail.com>
2019-12-14 23:17:38 -0300

    caralhoo

--- src/api/main.js
+++ src/api/main.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.API_URL || 'http://localhost:4500'
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4500'
 
 export default class MusicorumAPI {
   static getAuthStatus (token, full) {
