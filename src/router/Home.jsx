fdf8e8ce08ca13f31465bde322213116b8167a28
metehus <methidezeeni@gmail.com>
2020-01-05 22:49:58 -0300

    disclaimer

--- src/router/Home.jsx
+++ src/router/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function HomePage () {
   return (
-    <h1>Home page</h1>
+    <div>
+      <h1>Home page</h1>
+      This page is <b>Under Construction</b>. Please consider that some functions may not be working or having problems. Please report any issue at the official twitter account <a href="https://twitter.com/musicorumapp">@musicorumapp</a>
+    </div>
   )
 }
