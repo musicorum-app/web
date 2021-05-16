aebeaa32515b3d595d8b060b1f0458ef4baccb04
metehus <methidezeeni@gmail.com>
2021-05-16 02:06:01 -0300

    Icon fix

--- src/components/base/Base.js
+++ src/components/base/Base.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import '@fontsource/poppins/400.css'
-import '@fontsource/poppins/500.css'
-import '@fontsource/poppins/600.css'
-import '@fontsource/poppins/700.css'
-import { styled } from 'twin.macro'
-import '../../styles/base.scss'
+import React from "react"
+import "@fontsource/poppins/400.css"
+import "@fontsource/poppins/500.css"
+import "@fontsource/poppins/600.css"
+import "@fontsource/poppins/700.css"
+import { styled } from "twin.macro"
+import "../../styles/base.scss"
+import { Helmet } from "react-helmet"
 
-const Wrapper = styled.div`
-  
-`
 
-export default function Base ({ children }) {
-  return <Wrapper>{children}</Wrapper>
+export default function Base({ children }) {
+  return <div>
+    {children}
+  </div>
 }
